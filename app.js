f9b140e31680b760fac02df04622bb2366abd76e
iriemena <fortunateweb@yahoo.com>
2020-09-23 14:06:27 +0100

    menu-shop

---
+++ app.js
@@ -0,0 +1,312 @@
+// Arrays of menu
+let arr = [
+  {
+    id: 1,
+    img: "./images/foo1.jpg",
+    category: "food",
+    info: "Salad",
+    price: 20,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 2,
+    img: "./images/fru2.jpg",
+    category: "fruit",
+    info: "Oranges",
+    price: 14,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 3,
+    img: "./images/melon.jpg",
+    category: "fruit",
+    info: "Strawberry",
+    price: 15,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 4,
+    img: "./images/foo2.jpg",
+    category: "food",
+    info: "Salad",
+    price: 21,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 5,
+    img: "./images/fru1.jpg",
+    category: "fruit",
+    info: "Citrus Fruit",
+    price: 19,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 6,
+    img: "./images/foo3.jpg",
+    category: "food",
+    info: "Food",
+    price: 25,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 7,
+    img: "./images/fru3.jpg",
+    category: "fruit",
+    info: "Cherry",
+    price: 25,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 8,
+    img: "./images/cake1.jpg",
+    category: "cake",
+    info: "Cake",
+    price: 30,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 9,
+    img: "./images/cake2.jpg",
+    category: "cake",
+    info: "Cake",
+    price: 25,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 10,
+    img: "./images/cake3.jpg",
+    category: "cake",
+    info: "Cake",
+    price: 28,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 11,
+    img: "./images/wine1.jpg",
+    category: "wine",
+    info: "Wine",
+    price: 19,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 12,
+    img: "./images/wine2.jpg",
+    category: "wine",
+    info: "Wine",
+    price: 17,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+
+  {
+    id: 12,
+    img: "./images/wine3.jpg",
+    category: "wine",
+    info: "Wine",
+    price: 18,
+    text: `Lorem ipsum dolor sit amet, 
+		consectetur adipisicing elit, sed do 
+		eiusmod tempor incididunt ut labore et 
+		dolore magna aliqua. Ut enim ad minim veniam, 
+		quis nostrud exercitation ullamco laboris nisi 
+		ut aliquip ex ea commodo consequat. Duis aute 
+		irure dolor in reprehenderit in voluptate velit`,
+  },
+];
+
+// menu container
+let menu = document.querySelector(".container");
+
+// button container
+let buttonCat = document.querySelector(".button");
+
+document.addEventListener("DOMContentLoaded", function () {
+  displayMenu(arr);
+  displayCategory();
+});
+
+// displayBtn function
+function displayCategory() {
+  // displaying of button dynamically (1st option using reduce, filter & map method)
+
+  /*
+
+//selecting unique values and pushing "all" using reduce
+let cate = arr.reduce(function(value, item){
+	if(!value.includes(item.category)){
+		value.push(item.category)
+	}
+	return value;
+}, ['all'])
+
+//selecting the unique values using map method
+let category = cate.map(item=>{
+	return `<button class="btn all" data-id=${item}>${item}</button>`
+}).join('')
+buttonCat.innerHTML = category;
+
+//selecting the matched button using dataset & filter
+let button = document.querySelectorAll('.btn');
+button.forEach(btn =>{
+	btn.addEventListener('click', e =>{
+		let set = e.target.dataset.id;
+		let menuFil = arr.filter(item =>{
+			if(item.category === set){
+				return true;
+			}
+		})
+		if(set === 'all'){
+			displayMenu(arr)
+		}else{
+			displayMenu(menuFil)
+		}
+	})
+})
+*/
+
+  // displaying of button dynamically (2nd option using map, filter and set method)
+
+  // filtering categories using map
+  let menuCategory = arr.map(function (item) {
+    return item.category;
+  });
+
+  // filtering only the unique categories
+  let uniqueCategory = [...new Set(menuCategory)];
+
+  // adding "all" to category using unshift
+  uniqueCategory.unshift("all");
+
+  // displaying of the btn in the DOM
+  let displayBtn = uniqueCategory
+    .map(function (btn) {
+      return `<button class="btn all" data-id="${btn}">${btn}</button>`;
+    })
+    .join("");
+  buttonCat.innerHTML = displayBtn;
+
+  // filtering the menu with the matched button
+
+  // selecting each buuton
+  let button = document.querySelectorAll(".btn");
+
+  button.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // matching btn with its dataset
+      let data = e.target.dataset.id;
+
+      // filtering each category using filter method
+      let menuFilter = arr.filter(function (item) {
+        if (item.category === data) {
+          return item;
+        }
+      });
+      // inclusion of "all" since it is not in our array
+      if (data === "all") {
+        displayMenu(arr);
+      } else displayMenu(menuFilter);
+    });
+  });
+}
+
+// Menu display function
+function displayMenu(menuItem) {
+  let menuFilter = menuItem
+    .map((item) => {
+      return `<article class="menu">
+		<img class="photo" src=${item.img}>
+	<div class="header">
+		<header>
+		<h4 class="info">${item.info}</h4>
+		<h4 class="price">$${item.price}</h4>
+		</header>
+		<hr>
+		<p class="text">${item.text}</p>
+	</div>
+</article>`;
+    })
+    .join("");
+  menu.innerHTML = menuFilter;
+}
